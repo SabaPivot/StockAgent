e9b2dcdead235aa87ba297ba6ae9d0d5dc118205
SabaPivot <careforme.dropout@gmail.com>
2025-02-18 15:49:06 +0900

    Add: dotenv

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from chatbot import ChatbotAgent
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+os.environ["EXA_API_KEY"] = os.getenv("EXA_API_KEY")
+os.environ["OPENAI_API_KEY"] = os.getenv("OPEN_API_KEY")
 
 if __name__ == "__main__":
     agent = ChatbotAgent()
